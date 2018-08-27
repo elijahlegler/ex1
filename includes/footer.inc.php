a28954d90b65f463602f838b721994d08c1e1ef1
Elijah Legler <legler.elijah@gmail.com>
2018-08-27 15:35:13 -0400

    initial commit

---
+++ includes/footer.inc.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>Copyright 2018 elegler</p>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  include '/home/elegler/htdocs/CIT313/ex1/includes/header.inc.php';
+  ?>
+
+  <body>
+  <?php
+    $myArray = array('Elijah', 'Yellow', 'Inception', 'Algorithms to Live By', 'facebook.com');
+  ?>
+  <h1><?php echo $myArray[0];?></h1>
+
+  <ul>
+  <?php
+  for ($x = 1, $x < 5, $x = $x++) {
+    echo '<li>' . $myArray[$x] . '</li>';
+  }
+  ?>
+</ul>
+
+
+<?php
+  include '/home/elegler/htdocs/CIT313/ex1/includes/footer.inc.php';
+  ?>
